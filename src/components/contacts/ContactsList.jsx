4d5723e0ae010ad577a88e78b2782da9a2e9c5d0
Ali Gulmaliyev <ali.gulmaliyov@gmail.com>
2022-01-12 22:12:35 +0400

    contact info

--- src/components/contacts/ContactsList.jsx
+++ src/components/contacts/ContactsList.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
-import { Table, Space, Button, Modal } from "antd";
+import { Table, Card, Avatar, Space, Button, Modal } from "antd";
 import ContactContext from "../../context/contact/contact-context";
 import { Link } from "react-router-dom";
 import { Info, Delete, Edit, Add, Exclamation } from "../../assets/icons/index";
 import CustomModal from "../modal/Modal";
+import { capitalize } from "../../utils/Capitalize";
 
-const { confirm } = Modal;
+const { confirm, info } = Modal;
 
 const ContactsList = () => {
   const ctx = useContext(ContactContext);
@@ -37,12 +38,12 @@ const ContactsList = () => {
       key: "action",
       render: (text, contact) => (
         <Space size="middle">
-          <Button icon={<Info />} type="link" />
+          <CustomModal onClick={() => showInfoModal(contact)} icon={<Info />} />
           <Link to={"/contacts/edit/" + contact.key}>
             <Edit />
           </Link>
           <CustomModal
-            onClick={() => showDeleteConfirm(contact)}
+            onClick={() => showDeleteModal(contact)}
             icon={<Delete />}
           />
         </Space>
@@ -50,7 +51,7 @@ const ContactsList = () => {
     },
   ];
 
-  const showDeleteConfirm = (contact) => {
+  const showDeleteModal = (contact) => {
     const { key, name, lastName } = contact;
     confirm({
       title: "Are you sure delete this contact?",
@@ -63,6 +64,31 @@ const ContactsList = () => {
         ctx.removeContact(key);
       },
     });
+  };
+  const showInfoModal = (contact) => {
+    info({
+      title: "Contact Info",
+      icon: <Exclamation />,
+      content: showInfo(contact),
+      cancelText: "OK",
+    });
+  };
+  const showInfo = (contact) => {
+    return (
+      <Card
+        actions={[
+          <p key={contact.fatherName}>{capitalize(contact.fatherName)}</p>,
+          <p key={contact.gender}>{capitalize(contact.gender)}</p>,
+          <p key={contact.innovators}>{contact.innovators ? "Yes" : "No"}</p>,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+          title={capitalize(contact.name) + " " + capitalize(contact.lastName)}
+          description={capitalize(contact.profession)}
+        />
+      </Card>
+    );
   };
   return (
     <>
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -17,3 +17,48 @@ Modal.propTypes = {
 };
 
 export default Modal;
+
+// import React from "react";
+// import { Modal } from "antd";
+// import PropTypes from "prop-types";
+
+// const CustomModal = ({
+//   children,
+//   visible,
+//   title,
+//   okText,
+//   okType,
+//   cancelText,
+//   icon,
+//   onOk,
+//   onCancel,
+// }) => {
+//   return (
+//     <Modal
+//       visible={visible}
+//       okText={okText}
+//       okType={okType}
+//       cancelText={cancelText}
+//       title={title}
+//       onOk={onOk}
+//       onCancel={onCancel}
+//       closeIcon={icon}
+//     >
+//       {children}
+//     </Modal>
+//   );
+// };
+
+// CustomModal.propTypes = {
+//   children: PropTypes.node,
+//   title: PropTypes.string,
+//   okText: PropTypes.string,
+//   okType: PropTypes.string,
+//   cancelText: PropTypes.string,
+//   icon: PropTypes.element,
+//   onOk: PropTypes.func,
+//   visible: PropTypes.bool,
+//   onCancel: PropTypes.func,
+// };
+
+// export default CustomModal;
